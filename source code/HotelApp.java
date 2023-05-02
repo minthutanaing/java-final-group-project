6f49ed257bee24128e2d361f391b07636165dee7
Min Thuta Naing <122962303+minthutanaing@users.noreply.github.com>
2023-05-02 02:08:56 +0000

    1st commit

---
+++ source code/HotelApp.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HotelApp {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Room> rooms = new ArrayList<>(); 
+
+        for (int i = 1; i <= 30; i++){
+            Room room = new Room ("Single room", i);
+            rooms.add(room);
+        }
+        for (int i = 31; i <= 50; i++){
+            Room room = new Room ("Double room", i);
+            rooms.add(room);
+        }
+        for (int i = 51; i <= 55; i++){
+            Room room = new Room ("Suite", i);
+            rooms.add(room);
+        }
+
+
+        //System.out.println("These are the available lists of rooms.");
+        displayRoomTypes();
+        /*for (Room room: rooms){
+            if (room.isRoomAvailable()){
+                System.out.println(room.getRoomType() + " room " + room.getRoomNum());
+            }
+        }*/
+
+
+        System.out.print("Enter room type (single/double/suite): ");
+        String roomType = sc.nextLine();
+        System.out.print("Enter duration of stay (in days): ");
+        int duration = sc.nextInt();
+
+        // Check availability of rooms of the chosen type
+        boolean foundAvailableRoom = false;
+        for (Room room : rooms) {
+            if (room.getRoomType().equals(roomType) && room.isRoomAvailable()) {
+                room.setAvailability(false);
+                System.out.println("Room #" + room.getRoomNum() + " booked for " + duration + " days.");
+                foundAvailableRoom = true;
+                break;
+            }
+        }
+
+        if (!foundAvailableRoom) {
+            System.out.println("No available rooms of type " + roomType + ".");
+        }
+    }
+
+    static void displayRoomTypes(){
+        System.out.println("Choose the type of room: \n1. Single room \n2. Double Room \n3. Suite ");
+    }
+}
+
+
---
+++ source code/Room.java
@@ -0,0 +1,25 @@
+public class Room {
+    private String roomType;
+    private int roomNum;
+    private boolean availability;
+
+    public Room (String roomType, int roomNum){
+        this.roomType = roomType;
+        this.roomNum = roomNum;
+        this.availability = true;
+    }
+
+    public String getRoomType() {return roomType; }
+    public void setRoomType(String roomType) {this.roomType = roomType; }
+    public int getRoomNum() {return roomNum;}
+    public void setRoomNum(int roomNum) {this.roomNum = roomNum; }
+    public boolean isRoomAvailable() { 
+        return availability;
+    }
+
+    public void setAvailability(boolean availability){
+        this.availability = availability;
+    }
+
+
+}
